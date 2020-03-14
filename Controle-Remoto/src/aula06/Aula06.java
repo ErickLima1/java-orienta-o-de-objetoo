4f8751dfb95f4235b2a0b744442d7361f56cdd7a
ErickLima1 <58487117+ErickLima1@users.noreply.github.com>
2020-03-14 09:50:35 -0300

    Controle

---
+++ Controle-Remoto/src/aula06/Aula06.java
@@ -0,0 +1,21 @@
+
+package aula06;
+
+
+public class Aula06 {
+
+   
+    public static void main(String[] args) {
+     ControleRemoto c = new ControleRemoto();
+     c.ligar();
+     c.play();
+     //c.pause();
+    // c.maisvolume();
+     c.abrirmenu();
+     //c.fecharmenu();
+    // c.desligar();
+     
+     
+    }
+    
+}
