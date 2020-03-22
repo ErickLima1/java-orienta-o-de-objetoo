68ae513359114209118d163cc93f52513bdce874
ErickLima1 <58487117+ErickLima1@users.noreply.github.com>
2020-03-22 09:41:19 -0300

    Projeto-Elevador-Completo

---
+++ Projeto-Elevador/src/testeevelado/TesteEvelado.java
@@ -0,0 +1,14 @@
+
+package testeevelado;
+
+public class TesteEvelado {
+  public static void main(String[] args) {
+      Elevador E = new Elevador();
+      E.elevado();
+     
+        
+  } 
+}
+
+  
+
